3f831876cc401f2320c6938cd472d4d03ae07ae3
jack6099boy <jack6099boy@gmail.com>
2021-10-13 20:41:58 +0800

    add Dockerfile (TM5:GPU)

---
+++ Docker/gpu/tm_join.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#
+# Typical usage: ./join.bash subt
+#
+
+BASH_OPTION=bash
+
+IMG=jack6099boy/tm5:GPU16
+
+xhost +
+containerid=$(docker ps -aqf "ancestor=${IMG}") && echo $containerid
+docker exec -it \
+    --privileged \
+    -e DISPLAY=${DISPLAY} \
+    -e LINES="$(tput lines)" \
+    TM5 \
+    $BASH_OPTION
+xhost -
